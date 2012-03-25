52d74552f632bc41ca2ec5abc89514777951180f
zhengguotong <zhengguotong@gmail.com>
2012-03-26 10:58:30 +1100

    add php page

---
+++ php/index.php
@@ -0,0 +1,27 @@
+<!DCOTYPE html PUBLIC "-//W3C//DTD HTML 4.01/EN"
+  "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	 <head>
+	 	 <title>Online Travel Agency</title>
+	 </head>
+	 <body>
+	 	<?php include("menu.php"); ?>
+	 		
+	 	<div id="content">
+	 		<h4>welcome to online travel booking system</h4>
+	 		<p>
+	 			This website is created as an Assignment for the subject Programming on the internet at the University of 
+	 			Technology, Sydney for Auntumn semester 2012.  
+	 		</p>
+	 	</div>
+	 	
+	 	<div id="information">
+	 		
+	 	</div>
+	 	
+		<div id="footer">
+  		 Copyright &copy;  2012 Ben & Tony
+		</div>
+	 	
+	 </body>
+</html>
