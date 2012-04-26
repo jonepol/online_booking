69bebe616af776faebc81168670cad99c6eb5c1a
guotong zheng <zhengguotong@hotmail.com>
2012-04-26 15:28:02 +1000

    personal detail form validation

--- booking.js
+++ booking.js
@@ -26,14 +26,58 @@ function initAll()
 }
 
 
-function change_compulsory_filed(country)
+function personal_Detail_Validation()
 {
-	if (country.toLowerCase() == "australia")
+	//email address regular expression 
+    var emailReg = /^\w{1,15}(?:@(?!-))(?:(?:[a-z0-9-]*)(?:[a-z0-9](?!-))(?:\.(?!-)))+[a-z]{2,4}$/;
+	var postcodeReg = /[0-9]{4}/; // postcode regular expression 
+	var phoneReg = /[0-9]{0,20}/; //mobile regular expression
+
+	//check if the compulsory fileds are filled
+	if(document.getElementById("givenName").value == ""
+	   || document.getElementById("familyName").value == ""
+	   || document.getElementById("address1").value == ""
+	   || document.getElementById("country").value == ""
+	   || document.getElementById("emailAddress").value == "")
 	{
-		alert("australia");
+		alert("One or more compulsory fields is blank");
+		return false;
+	}
+	//if country is Australia then check if postcode, suburb and state are blank
+	else if (document.getElementById("country").value.toLowerCase() == "australia")
+	{   
+		var postcode = document.getElementById("postcode").value;
+
+		if(document.getElementById("suburb").value =="" 
+		   || document.getElementById("state").value == ""
+		   || postcode == "")
+		{
+			alert("One or more compulsory fields is blank");
+			return false;
+		}
+		//check if the postcode is correct
+		else if (!postcodeReg.test(postcode))
+		{
+			alert("Please enter a valid postcode");
+			document.getElementById("postcode").value = "";
+			return false;
+		}
+	}
+	//check if the emai is correct
+	else if (!emailReg.test(document.getElementById("emailAddress").value))
+	{
+		alert("Please enter a valid email address");
+		return false;
+	}
+	else if (!phoneReg.test(document.getElementById("businessPhone").value))
+	{
+		alert("Please enter a valid phone number");
+		document.getElementById("businessPhone").value = "";
+		return false;
 	}
 	else 
 	{
-		alert("others");
+		return true
 	}
+	
 }
